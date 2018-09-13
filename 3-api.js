c95f7defada4efeb52b310fe6da8dc216441a55d
Mahamat Zakaria Izadine <matamean@Izadine.local>
2018-09-12 20:14:12 -0400

    api

--- 3-api.js
+++ 3-api.js
@@ -1,5 +1,4 @@
 const baseURL = 'https://api.openaq.org/v1/countries';
-let url;
 
 const searchTerm = document.querySelector('.search');
 const submitBtn = document.querySelector('.submit');
@@ -12,58 +11,32 @@ searchForm.addEventListener('submit', fetchResults);
 function fetchResults(e) {
     e.preventDefault();
 
-    url = baseURL;
-
-
-    fetch(url)
+    fetch(baseURL)
         .then(function(result){
         return result.json();
     })
         .then(function(json){
-            // json[0][name];
         displayResults(json);
     });
 }
 
 function displayResults(json){
-    console.log(json.results)
+    console.log(json.results);
 
-    while (section.firstChild)
-        section.removeChild(section.firstChild);
+    while (section.firstChild){
+        section.removeChild(section.firstChild)};
     
     let search = json.results;
-//     if(search.length === 0) {
-//         console.log("No results");
-// }else{
+
+let current =searchTerm.value
+
     for (let i = 0; i < search.length; i++) {
-        // let name = document.createElement('name');
-        // let cities = document.createElement('cities');
-        // let code = document.createElement('code');
-        // let count = document.createElement('count');
-        // let clearfix = document.createElement('div');
-        // order_by[]=name&order_by[]=cities&order_by[]=code&order_by[]=count
-        
-        
-        let current = search[i].name;
-        console.log("current:", current);
-    }
-    //  if(search === ''){
-    //     console.log('');
-    // function displaySearch(){}
-        if(search !== 'current'){
-            console.log(searchTerm.value)
+        if(search[i].name == current){
+            console.log('result:',search[i])
+            return
         }else{
-            console.log('not a country')
-    }
+        }
+        console.log("not a country")
+    } 
 }
 
-        // link.href = current.web_url;
-
-
-// clearfix.setAttribute('class', 'clearfix');
-
-
-// name.appendChild(name);
-// cities.appendChild(cities);
-// code.appendChild(code);
-// count.appendChild(count);
